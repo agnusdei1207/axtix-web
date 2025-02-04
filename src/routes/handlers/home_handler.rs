81c138b1ce5504efc9ce1cd2363d78cd5907a865
psw <agnusdei1207@gmail.com>
2025-02-04 17:23:52 +0900

    chore update orm connect

--- src/routes/handlers/home_handler.rs
+++ src/routes/handlers/home_handler.rs
@@ -1,6 +1,7 @@
 use actix_web::{get, web, Responder};
+use sea_orm::{ConnectionTrait, QueryResult, Statement};
 
-use crate::utils::api_response;
+use crate::utils::{api_response, app_state::AppState};
 
 #[get("/hello/{name}")]
 pub async fn greet(name: web::Path<String>) -> impl Responder {
@@ -8,6 +9,14 @@ pub async fn greet(name: web::Path<String>) -> impl Responder {
 }
 
 #[get("/test")]
-pub async fn test() -> impl Responder {
+pub async fn test(app_state: web::Data<AppState>) -> impl Responder {
+    let res: Vec<QueryResult> = app_state
+        .db
+        .query_all(Statement::from_string(
+            sea_orm::DatabaseBackend::Postgres,
+            "SELECT * FROM user;",
+        ))
+        .await
+        .unwrap();
     api_response::ApiResponse::new(200, "Test".to_string())
 }
