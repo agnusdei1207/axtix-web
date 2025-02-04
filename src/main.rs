8d10f2c0044ed3d35d8c76df6d5f092d1aa858ae
psw <agnusdei1207@gmail.com>
2025-02-04 12:04:07 +0900

    chore update handler

--- src/main.rs
+++ src/main.rs
@@ -5,26 +5,20 @@ use env_logger;
 mod routes;
 mod utils;
 
-// GET 요청을 처리하는 핸들러 함수
-// 요청 URL: /hello/{name} -> {name} 값이 동적으로 전달됨
-#[get("/hello/{name}")]
-async fn greet(name: web::Path<String>) -> impl Responder {
-    // 클라이언트에게 "Hello {name}!" 형식의 문자열을 응답으로 보냄
-    format!("Hello {name}!")
-}
-
 #[actix_web::main] // Actix의 비동기 런타임 매크로 (메인 함수가 비동기로 실행됨)
 async fn main() -> std::io::Result<()> {
-    // .env 파일을 로드하여 환경 변수 사용 가능하게 설정
-    dotenv().ok();
-
     // 환경 변수 RUST_LOG가 설정되지 않았다면 기본값을 "actix_web=info"로 설정
     if std::env::var_os("RUST_LOG").is_none() {
         std::env::set_var("RUST_LOG", "actix_web=info");
     }
 
+    // .env 파일을 로드하여 환경 변수 사용 가능하게 설정
+    dotenv().ok();
     // env_logger 초기화 (로그 시스템 활성화)
     env_logger::init();
+
+    let address: String = (utils::constants::ADDRESS).clone();
+    let port: u16 = (utils::constants::PORT).clone();
 
     // HTTP 서버 생성 및 실행
     HttpServer::new(|| {
@@ -32,11 +26,8 @@ async fn main() -> std::io::Result<()> {
         App::new()
             // 로깅 미들웨어 추가 (요청 정보를 로그로 남김)
             .wrap(Logger::default())
-            // "greet" 핸들러를 서비스로 등록
-            .service(greet)
     })
-    // 서버를 127.0.0.1(로컬호스트) 8080번 포트에 바인딩
-    .bind(("127.0.0.1", 8080))?
+    .bind((address, port))?
     // 서버 실행 (비동기 처리)
     .run()
     .await
