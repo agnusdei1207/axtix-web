e32839d268f7be236f5d2674a2545a876bb50981
psw <agnusdei1207@gmail.com>
2025-02-04 20:45:39 +0900

    chore signup

--- src/routes/auth_routes.rs
+++ src/routes/auth_routes.rs
@@ -3,8 +3,9 @@ use actix_web::web;
 use super::handlers;
 
 pub fn config(config: &mut web::ServiceConfig) {
-    config
-        .service(web::scope("/auth"))
-        .service(handlers::auth_handler::register)
-        .service(handlers::auth_handler::login);
+    config.service(
+        web::scope("/auth")
+            .service(handlers::auth_handler::register)
+            .service(handlers::auth_handler::login),
+    );
 }
