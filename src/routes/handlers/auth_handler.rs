c45035e80cea52ba9b79645d3feaace51279800e
psw <agnusdei1207@gmail.com>
2025-02-04 20:54:08 +0900

    chore update

--- src/routes/handlers/auth_handler.rs
+++ src/routes/handlers/auth_handler.rs
@@ -1,19 +1,15 @@
 // `api_response`는 API 응답을 표준화하는 유틸리티이고, `AppState`는 애플리케이션의 상태(예: 데이터베이스 연결)를 관리합니다.
 use crate::utils::{api_response, app_state::AppState};
 
+use actix_web::{post, web, Responder};
+use sea_orm::ActiveModelTrait;
 use sea_orm::ColumnTrait;
 use sea_orm::Condition;
-// `Responder`는 HTTP 응답을 생성하는 트레이트입니다.
-use actix_web::{post, web, Responder};
-
-// `Set`은 필드 값을 설정할 때 사용됩니다.
-use sea_orm::ActiveModelTrait;
 use sea_orm::EntityTrait;
 use sea_orm::QueryFilter;
 use sea_orm::Set;
-
-// `Deserialize`는 JSON 데이터를 Rust 구조체로 변환할 때 사용됩니다.
 use serde::Deserialize;
+use sha256::digest;
 
 #[derive(Deserialize)]
 struct RegisterModel {
@@ -37,7 +33,7 @@ pub async fn register(
     let user_model: entity::user::Model = entity::user::ActiveModel {
         name: Set(data.name),
         email: Set(data.email),
-        password: Set(data.password),
+        password: Set(digest(data.password)),
         ..Default::default()
     }
     .insert(&app_state.db)
@@ -52,7 +48,7 @@ pub async fn login(
     app_state: web::Data<AppState>,
     login_json: web::Json<LoginModel>,
 ) -> impl Responder {
-    let user = entity::user::Entity::find()
+    let user: Option<entity::user::Model> = entity::user::Entity::find()
         .filter(
             Condition::all()
                 .add(entity::user::Column::Email.eq(&login_json.email))
@@ -61,6 +57,8 @@ pub async fn login(
         .one(&app_state.db)
         .await
         .unwrap();
+
+    dbg!(&user);
 
     if user.is_none() {
         return api_response::ApiResponse::new(404, "User not found".to_string());
