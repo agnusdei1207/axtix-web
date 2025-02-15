3e432efd4b1a8786283cd23b9c4945684476b041
psw <agnusdei1207@gmail.com>
2025-02-15 16:00:48 +0900

    chore update user routes

--- src/routes/user_routes.rs
+++ src/routes/user_routes.rs
@@ -8,6 +8,7 @@ pub fn config(config: &mut web::ServiceConfig) {
     config.service(
         web::scope("/user")
             .wrap(from_fn(middlewares::auth_middleware::check_auth_middleware))
-            .service(handlers::user_handler::user),
+            .service(handlers::user_handler::user)
+            .service(handlers::user_handler::update_user),
     );
 }
