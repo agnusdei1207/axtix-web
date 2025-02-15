72f26a0e144ccd80c0ebde9c9d57bcc0a29da711
psw <agnusdei1207@gmail.com>
2025-02-15 14:42:30 +0900

    chore update user handler

--- src/routes/handlers/user_handler.rs
+++ src/routes/handlers/user_handler.rs
@@ -1,7 +1,13 @@
 use crate::utils::{api_response, app_state, jwt::Claims};
-use actix_web::{get, web, Error, HttpResponse, Responder};
-use sea_orm::EntityTrait;
-use serde_json::json;
+use actix_web::{delete, get, post, web, Responder};
+use sea_orm::ActiveModelTrait;
+use sea_orm::{EntityTrait, IntoActiveModel, Set};
+use serde::{Deserialize, Serialize};
+
+#[derive(Serialize, Deserialize)]
+struct UpdateUserModel {
+    name: String,
+}
 
 #[get("")]
 pub async fn user(
@@ -11,11 +17,45 @@ pub async fn user(
     let user_model = entity::user::Entity::find_by_id(claim_data.id)
         .one(&app_state.db)
         .await
-        .map_err(|err| actix_web::error::ErrorInternalServerError(err.to_string()))?
-        .ok_or(|| actix_web::error::ErrorNotFound("User not found".to_owned()))?;
+        .map_err(|err| api_response::ApiResponse::new(500, err.to_string()))?
+        .ok_or(api_response::ApiResponse::new(
+            404,
+            "User Not Found".to_owned(),
+        ))?;
 
     Ok(api_response::ApiResponse::new(
         200,
-        "Verified User".to_string(),
+        format!(
+            " {{ 'name': '{}', 'email': '{}' }} ",
+            user_model.name, user_model.email
+        ),
+    ))
+}
+
+#[post("update")]
+pub async fn update_user(
+    app_state: web::Data<app_state::AppState>,
+    user_data: web::Json<UpdateUserModel>,
+    claim_data: Claims,
+) -> Result<api_response::ApiResponse, api_response::ApiResponse> {
+    let mut user_model = entity::user::Entity::find_by_id(claim_data.id)
+        .one(&app_state.db)
+        .await
+        .map_err(|err| api_response::ApiResponse::new(500, err.to_string()))?
+        .ok_or(api_response::ApiResponse::new(
+            404,
+            "User Not Found".to_owned(),
+        ))?
+        .into_active_model();
+
+    user_model.name = Set(user_data.name.clone());
+    user_model
+        .update(&app_state.db)
+        .await
+        .map_err(|err| api_response::ApiResponse::new(500, err.to_string()))?;
+
+    Ok(api_response::ApiResponse::new(
+        200,
+        "User Updated".to_owned(),
     ))
 }
